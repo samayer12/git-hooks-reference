0c985358077390416cbecf3322611341e08626cd
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:35 -0600

    10: Create framework for standard color in log messages

---
+++ scripts/use-color-text.sh
@@ -0,0 +1,10 @@
+source color-text.sh
+
+print_log_message "EMERGENCY" "The message"
+print_log_message "ALERT" "The message"
+print_log_message "CRITICAL" "The message"
+print_log_message "ERROR" "The message"
+print_log_message "WARNING" "The message"
+print_log_message "NOTICE" "The message"
+print_log_message "INFORMATIONAL" "The message"
+print_log_message "DEBUG" "The message"
