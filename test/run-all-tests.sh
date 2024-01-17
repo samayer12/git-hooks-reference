1c3e7418df388573ca3b08a4eda151e3ca7afd1a
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:33 -0600

    6: Create reporting mechanism for status of test runs

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+rm test-report.log # Clear report file between runs
+
 for TEST_FILE in test-*.sh; do
   bash "$TEST_FILE"
 done
+
+PASS_COUNT=$(grep -o -i "PASS" test-report.log | wc -l)
+FAIL_COUNT=$(grep -o -i "FAIL" test-report.log | wc -l)
+printf "INFO - Tests complete, %d passed and %d failed. See logs for details." "$PASS_COUNT" "$FAIL_COUNT"
+
+[[ $FAIL_COUNT -gt 0 ]] &&  exit 1 # Report failure if any test failed
