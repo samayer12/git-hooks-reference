d55022fb17d1264d77eaf802cf660c4fc3959c9c
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:29 -0600

    3: Start work on prepare-commit-msg test scaffolding

--- test/test-prepare-commit-msg.sh
+++ test/test-prepare-commit-msg.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-declare -a TEST_INPUT=( \
-  "Prepare Commit Input 1" \
-  "Prepare Commit Input 2" \
+COUNT=0
+LOG_FILE_PREFIX='test-prepare-commit-msg'
+LOG_DIR='logs'
+
+run_test_cases(){
+  mkdir -p $LOG_DIR
+  declare -a TEST_CASES=("$@")
+  for TEST_CASE in "${!TEST_CASES[@]}";
+  do
+    RESULT=''
+    if ./call-prepare-commit-msg -i "${TEST_CASES[TEST_CASE]}" > "logs/$LOG_FILE_PREFIX-$COUNT-output.log" 2>&1; then
+      RESULT='PASS'
+    else
+      RESULT='FAIL'
+    fi
+
+    printf "CASE %d - %s - Input: '%s'\n" "$COUNT" "$RESULT" "${TEST_CASES[TEST_CASE]}"
+    COUNT=$((COUNT+1))
+
+  done
+
+  rm test-*.txt # Remove temporary test files
+}
+
+
+declare -a COMMIT_MESSAGES=( \
+  '123: Prepare test message 0' \
+  '456: Prepare test message 1' \
 )
 
-for TEST_ITEM in "${TEST_INPUT[@]}";
-do
-  echo "$TEST_ITEM"
-done
+# TODO: Stage a file that gets linted
+
+run_test_cases "${COMMIT_MESSAGES[@]}"
