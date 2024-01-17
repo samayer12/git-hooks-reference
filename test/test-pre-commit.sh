8370d4d52cd273aee5952669b0364b70d7feb332
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:29 -0600

    3: Wire up pre-commit hook, need to create useful test data

--- test/test-pre-commit.sh
+++ test/test-pre-commit.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-declare -a TEST_INPUT=( \
-  "Pre Commit Input 1" \
-  "Pre Commit Input 2" \
+COUNT=0
+LOG_FILE_PREFIX='test-pre-commit-case'
+LOG_DIR='logs'
+
+run_test_cases(){
+  mkdir -p $LOG_DIR
+  declare -a TEST_CASES=("$@")
+  for TEST_CASE in "${!TEST_CASES[@]}";
+  do
+    RESULT=''
+    if ./call-pre-commit -i "${TEST_CASES[TEST_CASE]}" > "logs/$LOG_FILE_PREFIX-$COUNT-output.log" 2>&1; then
+      RESULT='PASS'
+    else
+      RESULT='FAIL'
+    fi
+
+    printf "CASE %d - %s - Input: '%s'\n" "$COUNT" "$RESULT" "${TEST_CASES[TEST_CASE]}"
+    COUNT=$((COUNT+1))
+
+  done
+
+  rm test-*.txt # Remove temporary test files
+}
+
+
+declare -a COMMIT_MESSAGES=( \
+  '123: Test message 0' \
+  '456: Test message 1' \
 )
 
-for TEST_ITEM in "${TEST_INPUT[@]}";
-do
-  echo "$TEST_ITEM"
-done
+# TODO: Stage a file that gets linted
+
+run_test_cases "${COMMIT_MESSAGES[@]}"
