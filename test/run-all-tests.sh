9181bffa258a40b49ed701d02cff7f95ea45886f
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:34 -0600

    9: Update log messages when CI environment is detected

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -4,10 +4,13 @@ rm test-report.log # Clear report file between runs
 
 printf "INFO - Starting test suite.\n"
 
-FAILURE_THRESHOLD=$([[ -n $CI ]] && { printf "WARN - CI Environment detected, updating failure threshold due to CI limitations." && echo 3; } \
-                  ||  { echo 0; })
+FAILURE_THRESHOLD=0
 
-printf "INFO - Failure Threshold allows %s test failures.\n" "$FAILURE_THRESHOLD"
+if [[ -n $CI ]]; then
+  FAILURE_THRESHOLD=3
+  printf "WARN - CI Environment detected, updating failure threshold to %d due to CI limitations.\n" \
+         "$FAILURE_THRESHOLD"
+fi
 
 for TEST_FILE in test-*.sh; do
   bash "$TEST_FILE"
@@ -15,6 +18,8 @@ done
 
 PASS_COUNT=$(grep -o -i "PASS" test-report.log | wc -l)
 FAIL_COUNT=$(grep -o -i "FAIL" test-report.log | wc -l)
-printf "INFO - Tests complete, %d passed and %d failed. See logs for details." "$PASS_COUNT" "$FAIL_COUNT"
+printf "INFO - Tests complete, %d passed and %d failed. See logs for details." \
+       "$PASS_COUNT" \
+       "$FAIL_COUNT"
 
-[[ $FAIL_COUNT -gt $FAILURE_THRESHOLD ]] &&  exit 1 # Report failure if any test failed
+[[ $FAIL_COUNT -gt $FAILURE_THRESHOLD ]] &&  exit 1
