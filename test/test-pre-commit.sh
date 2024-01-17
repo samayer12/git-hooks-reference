fca7dfb9f32747c55a267a70bf1b8ad91df7461b
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:27 -0600

    1: Rename input for tests

--- test/test-pre-commit.sh
+++ test/test-pre-commit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 declare -a TEST_INPUT=( \
-  "Input 1" \
-  "Input 2" \
+  "Pre Commit Input 1" \
+  "Pre Commit Input 2" \
 )
 
 for TEST_ITEM in "${TEST_INPUT[@]}";
--- test/test-prepare-commit-msg.sh
+++ test/test-prepare-commit-msg.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 declare -a TEST_INPUT=( \
-  "Input 1" \
-  "Input 2" \
+  "Prepare Commit Input 1" \
+  "Prepare Commit Input 2" \
 )
 
 for TEST_ITEM in "${TEST_INPUT[@]}";
