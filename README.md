d7179d051b2b9911f002f3b3d562572c0318e910
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:31 -0600

    4: Add happy-path test case for pre-push

--- README.md
+++ README.md
@@ -25,7 +25,8 @@ Key files:
 
 ## Installation
 
-Within a particular ecosystem, there may be a common way of installing things, such as using Yarn, NuGet, or Homebrew. However, consider the possibility that whoever is reading your README is a novice and would like more guidance. Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible. If it only runs in a specific context like a particular programming language version or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.
+You'll need to replace file paths in the `hooks/` directory with the correct paths for your project.
+For example, within `pre-push` there is a `FRONTEND_DIRECTORY` and a `BACKEND_DIRECTORY` variable that default to `"test"`.
 
 ## Usage
 
--- test/test-pre-push.sh
+++ test/test-pre-push.sh
@@ -28,7 +28,21 @@ run_test_cases(){
 
 declare -a COMMIT_MESSAGES=( \
   "123: Pre Push Input 1" \
-  "456: Pre Push Input 2" \
+  # "456: Pre Push Input 2" \ # Need to make a test case for sad path
 )
 
+CURRENT_SHA="$(git rev-parse HEAD)"
+touch index.ts # A dummy frontend file
+touch main.java # A dummy backend file
+git add main.java index.ts
+git commit -m "123123123: A change to backend and frontend" # Need to automate the 'y' prompt or branch appropriately
+# Cannot just branch without also pushing and then deleting that branch because of rev-parse with origin/HEAD
+
 run_test_cases "${COMMIT_MESSAGES[@]}"
+
+git reset "$CURRENT_SHA"
+rm main.java index.ts
+
+[[ $CURRENT_SHA -eq $(git rev-parse HEAD) ]]; \
+  (echo "INFO - Commit SHA reset success."; exit 0) || \
+  (echo "ERROR - Commit SHA mismatch after running pre-push tests."; exit 1)
