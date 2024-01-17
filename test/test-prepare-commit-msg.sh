d31892694a1a598ca2ae286eeee5bc6cea7d2b87
Sam Mayer <samayer12@gmail.com>
2024-01-16 19:28:32 -0600

    6: Suppress output for git command in test setup/teardown

--- test/test-prepare-commit-msg.sh
+++ test/test-prepare-commit-msg.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # TODO: Stage a file that gets linted
-git checkout -b '123456789-test-branch-prepare-commit'
-echo "Current Branch: $(git symbolic-ref --short HEAD)"
+git checkout -b '123456789-test-branch-prepare-commit' 1>/dev/null 2>&1
+echo "Current Branch: $(git symbolic-ref --short HEAD)" 1>/dev/null 2>&1
 
 ./run-test-cases.sh --directory logs --input "input/valid-prepare-commit-msg.txt" --hook-name prepare-commit-msg
 
-git checkout main
-git branch -D '123456789-test-branch-prepare-commit'
+git checkout main 1>/dev/null 2>&1
+git branch -D '123456789-test-branch-prepare-commit' 1>/dev/null 2>&1
